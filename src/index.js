2004a1d0d7dbcdcca1321b7ae2ae946b18c6f9be
Rajnigandha Jadhav <jadhavrajnigandha@gmail.com>
2022-09-29 11:55:05 +0530

    commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("./routes/route.js");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://functionup-cohort:9QsFpQHzVpmlSepb@cluster0.fmzaohu.mongodb.net/group8Database?retryWrites=true&w=majority",{
+      useNewUrlParser: true, 
+    })
+
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
+
+
+app.use("/", route);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
+});

9dea665314bc3e4ec671ca26850e1c3e08edc1bd
Functionup-sg <sujatagarande@gmail.com>
2022-09-29 12:26:01 +0530

    created route

---
+++ src/routes/route.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const router=express.Router();
+const urlController=require('../controller/urlController')
+
+
+
+//.......................url Apis.....................................//
+router.post('/url/shorten',urlController.createShortUrl)
+router.get('/urlCode', urlController.getUrlCode)
+
+
+
+
+//......................invalid params....................................//
+router.all('*',function (req,res){res.status(400).send({msg:"this page does not exist"})})
+
+module.exports=router
